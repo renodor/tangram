b14e3316bf97c9bea800c8c2ed77b5a975272b8e
renaud dor <renaud.dor@gmail.com>
2022-10-21 17:43:43 +0200

    Refacto polygonBuilder

--- polygonBuilder.js
+++ polygonBuilder.js
@@ -2,10 +2,12 @@ import * as THREE from 'three';
 import { Vector2 } from 'three';
 
 export default ({ type, size, name }) => {
+  const hypotenuse = size * Math.sqrt(2)
+
   // SHAPE
   const shape  = new THREE.Shape()
-  const points = buildPoints(type, size)
-  points.forEach(([x, y]) => shape.lineTo(x, y))
+  const points = buildPoints(type, size, hypotenuse)
+  points.forEach(([x, y], index) => { index == 0 ? shape.moveTo(x,y) : shape.lineTo(x, y) })
 
   // BOTTOM
   const bottomGeometry = new THREE.ShapeGeometry(shape);
@@ -28,95 +30,134 @@ export default ({ type, size, name }) => {
   const mainMesh = new THREE.Mesh(mainGeometry, mainMaterial)
   mainMesh.userData.type = 'main'
 
-  // Center polygon
-  const hypotenuse = size * Math.sqrt(2)
-  if (type == 'triangle') {
-    bottomGeometry.translate(0, -((hypotenuse / 2) / 3), 0)
-    mainGeometry.translate(0, -((hypotenuse / 2) / 3), 0)
-    topGeometry.translate(0, -((hypotenuse / 2) / 3), 0)
-  } else if (type == 'cube') {
-    bottomGeometry.translate(- (size / 2), - (size / 2), 0)
-    mainGeometry.translate(- (size / 2), - (size / 2), 0)
-    topGeometry.translate(- (size / 2), - (size / 2), 0)
-  }
+  const displayedPoints = new THREE.Points(mainGeometry)
 
   // POLYGON
   const polygon = new THREE.Group()
+  polygon.userData.type = type
   polygon.name = name
   polygon.add(bottomMesh)
   polygon.add(topMesh)
   polygon.add(mainMesh)
+  // polygon.add(displayedPoints)
 
-  let originalPoints = []
-  if (type == 'triangle') {
-    // Add more points to triangle to detect collisions
-    originalPoints.push(points[0])
-    originalPoints.push([points[0][0] + 0.5, 0])
-    originalPoints.push([points[0][0] + 1, 0])
-    originalPoints.push([points[0][0] + 1.5, 0])
-
-    originalPoints.push([0, 0])
-
-    originalPoints.push([points[1][0] - 1.5, 0])
-    originalPoints.push([points[1][0] - 1, 0])
-    originalPoints.push([points[1][0] - 0.5, 0])
-    originalPoints.push(points[1])
-    originalPoints.push([points[1][0] - 0.5, points[1][1] + 0.5])
-    originalPoints.push([points[1][0] - 1, points[1][1] + 1])
-    originalPoints.push([points[1][0] - 1.5, points[1][1] + 1.5])
-
-    originalPoints.push([hypotenuse / 4, hypotenuse / 4])
-
-    originalPoints.push([points[2][0] + 1.5, points[2][1] - 0.5])
-    originalPoints.push([points[2][0] + 1, points[2][1] - 1])
-    originalPoints.push([points[2][0] + 0.5, points[2][1] - 1.5])
-    originalPoints.push(points[2])
-    originalPoints.push([points[2][0] - 0.5, points[2][1] - 0.5])
-    originalPoints.push([points[2][0] - 1, points[2][1] - 1])
-    originalPoints.push([points[2][0] - 1.5, points[2][1] - 1.5])
-
-    originalPoints.push([-(hypotenuse / 4), hypotenuse / 4])
-
-    originalPoints.push([points[0][0] + 1.5, points[0][1] + 1.5])
-    originalPoints.push([points[0][0] + 1, points[0][1] + 1])
-    originalPoints.push([points[0][0] + 0.5, points[0][1] + 0.5])
-    originalPoints.push(points[0])
-
-    originalPoints = originalPoints.map(([x, y]) => [x, y - ((hypotenuse / 2) / 3)])
-  } else if (type == 'cube') {
-    // Add more points to cube to detect collisions
-    originalPoints.push(points[0])
-    originalPoints.push([size / 2, 0])
-    originalPoints.push(points[1])
-    originalPoints.push([size, size / 2])
-    originalPoints.push(points[2])
-    originalPoints.push([size / 2, size])
-    originalPoints.push(points[3])
-    originalPoints.push([0, size / 2])
-    originalPoints.push(points[0])
-
-    originalPoints = originalPoints.map(([x, y]) => [x - (size / 2), y - (size / 2)])
-  }
-  polygon.userData.originalPoints = originalPoints
-  polygon.userData.currentPoints = originalPoints
+  centerPolygon(polygon, size, hypotenuse)
+  addCollisionPointsToPolygon(polygon, points, size, hypotenuse)
 
   return polygon
 }
 
-const buildPoints = (type, size) => {
-  if (type == 'cube') {
-    return [
-      [0, 0],
-      [size, 0],
-      [size, size],
-      [0, size]
-    ]
-  } else if (type == 'triangle') {
-    const hypotenuse = size * Math.sqrt(2)
-    return [
-      [-(hypotenuse / 2), 0],
-      [0, hypotenuse / 2],
-      [(hypotenuse / 2), 0],
-    ]
+const buildPoints = (type, size, hypotenuse) => {
+  switch (type) {
+    case 'triangle':
+      return [
+        [-(hypotenuse / 2), 0],
+        [(hypotenuse / 2), 0],
+        [0, hypotenuse / 2]
+      ]
+    case 'cube':
+      return [
+        [0, 0],
+        [size, 0],
+        [size, size],
+        [0, size]
+      ]
   }
+}
+
+const centerPolygon = (polygon, size, hypotenuse) => {
+  switch (polygon.userData.type) {
+    case 'triangle':
+      polygon.children.forEach((children) => children.geometry.translate(...centerTrianglePoint([0, 0, 0], hypotenuse)))
+      break;
+    case 'cube':
+      polygon.children.forEach((children) => children.geometry.translate(...centerCubePoint([0, 0, 0], size)))
+      break;
+  }
+}
+const centerTrianglePoint = ([x, y, z], hypotenuse) => ([x, y - ((hypotenuse / 2) / 3), z])
+const centerCubePoint = ([x, y, z], size) => ([x - (size / 2), y - (size / 2), z])
+
+const addCollisionPointsToPolygon = (polygon, points, size, hypotenuse) => {
+  // Add more points to polygon to detect collisions
+  let collisionPoints = []
+  switch (polygon.userData.type) {
+    case 'triangle':
+      points.forEach(([x, y], index) => {
+        collisionPoints.push()
+        switch (index) {
+          case 0:
+            collisionPoints.push(
+              [x, y],
+              [x + 0.5, 0],
+              [x + 1, 0],
+              [x + 1.5, 0],
+              [0, 0],
+            )
+            break;
+          case 1:
+            collisionPoints.push(
+              [x - 1.5, 0],
+              [x - 1, 0],
+              [x - 0.5, 0],
+              [x, y],
+              [x - 0.5, y + 0.5],
+              [x - 1, y + 1],
+              [x - 1.5, y + 1.5],
+              [hypotenuse / 4, hypotenuse / 4]
+            )
+            break;
+          case 2:
+            collisionPoints.push(
+              [x + 1.5, y - 1.5],
+              [x + 1, y - 1],
+              [x + 0.5, y - 0.5],
+              [x, y],
+              [x - 0.5, y - 0.5],
+              [x - 1, y - 1],
+              [x - 1.5, y - 1.5],
+              [-(hypotenuse / 4), hypotenuse / 4],
+              [points[0][0] + 1.5, points[0][1] + 1.5],
+              [points[0][0] + 1, points[0][1] + 1],
+              [points[0][0] + 0.5, points[0][1] + 0.5],
+            )
+        }
+      })
+
+      collisionPoints.push(points[0])
+      collisionPoints = collisionPoints.map(([x, y]) => centerTrianglePoint([x, y, 0], hypotenuse))
+      break;
+    case 'cube':
+      points.forEach(([x, y], index) => {
+        collisionPoints.push()
+        switch (index) {
+          case 0:
+            collisionPoints.push(
+              [x, y],
+              [size / 2, 0]
+            )
+          case 1:
+            collisionPoints.push(
+              [x, y],
+              [size, size / 2]
+            )
+          case 2:
+            collisionPoints.push(
+              [x, y],
+              [size / 2, size]
+            )
+          case 3:
+            collisionPoints.push(
+              [x, y],
+              [0, size / 2],
+            )
+        }
+      })
+
+      collisionPoints.push(points[0])
+      collisionPoints = collisionPoints.map(([x, y]) => centerCubePoint([x, y, 0], size))
+      break;
+  }
+  polygon.userData.originalPoints = collisionPoints
+  polygon.userData.currentPoints = collisionPoints
 }
