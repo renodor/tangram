edd94e2f6251f4ea0d0ea58a0dd723338071c440
renaud dor <renaud.dor@gmail.com>
2022-12-16 16:09:08 +0100

    Create winning modal pattern animation

--- app/javascript/controllers/winning_modal_controller.js
+++ app/javascript/controllers/winning_modal_controller.js
@@ -4,11 +4,11 @@ export default class extends Controller {
   static targets = ['body']
 
   close() {
-    this.element.classList.add('display-none')
+    this.element.dataset.open = false
   }
 
   open({ detail: { svgTag } }) {
     this.bodyTarget.innerHTML = svgTag
-    this.element.classList.remove('display-none')
+    this.element.dataset.open = true
   }
 }
