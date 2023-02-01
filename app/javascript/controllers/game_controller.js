968d207afd96a2727d2ce17db205536fd2ac7db8
renaud dor <renaud.dor@gmail.com>
2023-02-01 12:25:17 +0100

    Correct checkPattern()

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -342,7 +342,7 @@ export default class extends Controller {
       return this.comparePolygonsSolution(solution)
     })
 
-    if (!polygonsMatchSolution) {
+    if (polygonsMatchSolution) {
       console.log(`Bravo you found a pattern!`)
 
       if (!this.currentPatternSolvedValue) {
